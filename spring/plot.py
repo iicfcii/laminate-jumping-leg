4944034f3e918d20103d29053f384fc3a3762944
iicfcii <8230152+iicfcii@users.noreply.github.com>
2021-12-02 11:22:00 -0700

    Added more spring plots

--- spring/plot.py
+++ spring/plot.py
@@ -8,34 +8,46 @@ import process
 
 plt.figure()
 for i, thickness in enumerate([15,20]):
-    ps0 = []
-    tzs0 = []
-    for trial in [1,2]:
-        fs = data.read('../data/{:d}mil_40mm_20mm_1_{:d}.csv'.format(thickness,trial))
+    for j, length in enumerate([25,50]):
+        c = 'C{:d}'.format(i)
+        if length == 25:
+            l = '-'
+            d = (None,None)
+            gap = 4
+        else:
+            l = '--'
+            d = (10,10)
+            gap = 2
 
-        ps0.append(np.array(fs['p']))
-        tzs0.append(np.array(fs['tz']))
+        ps0 = []
+        tzs0 = []
+        for sample in [1]:
+            for trial in [1]:
+                fs = data.read('../data/{:d}mil_{:d}mm_20mm_{:d}_{:d}.csv'.format(thickness,length+15,sample,trial))
+                ps0.append(np.array(fs['p']))
+                tzs0.append(np.array(fs['tz']))
 
-    ps0 = np.array(ps0).flatten()
-    tzs0 = np.array(tzs0).flatten()
-    ps, tzs = process.remove_gap(ps0,tzs0)
+        ps0 = np.concatenate(ps0)
+        tzs0 = np.concatenate(tzs0)
+        ps, tzs = process.remove_gap(ps0,tzs0,gap)
 
-    # # Raw and adjusted data
-    # plt.figure()
-    # plt.plot(ps0,tzs0,label='raw')
-    # plt.plot(ps,tzs,'.',label='adjusted')
-    # plt.xlabel('Position Count (0.088deg/count)')
-    # plt.ylabel('Base Torque [Nm]')
-    # plt.legend()
-    # plt.show()
+        # # Raw and adjusted data
+        # plt.figure()
+        # plt.plot(ps0,tzs0,label='raw')
+        # plt.plot(ps,tzs,'.',label='adjusted')
+        # plt.xlabel('Position Count (0.088deg/count)')
+        # plt.ylabel('Base Torque [Nm]')
+        # plt.legend()
+        # plt.show()
 
-    pv, tv = process.base2virtual(ps,tzs)
-    k = np.linalg.lstsq(pv.reshape(-1,1),tv,rcond=None)[0][0]
-    tvfit = pv*k
+        pv, tv = process.base2virtual(ps,tzs,length/1000)
+        k = np.linalg.lstsq(pv.reshape(-1,1),tv,rcond=None)[0][0]
 
-    c = 'C{:d}'.format(i)
-    plt.plot(pv,tv,'.',color=c,markersize=0.5)
-    plt.plot(pv,tvfit,color=c,label='k={:.3f} t={}'.format(k,thickness))
+        pvfit = np.array([np.amin(pv),np.amax(pv)])
+        tvfit = pvfit*k
+
+        plt.plot(pv,tv,'.',color=c,markersize=0.2)
+        plt.plot(pvfit,tvfit,l,color=c,dashes=d,label='k={:.3f} t={} l={}'.format(k,thickness,length))
 
 plt.xlabel('Virtual Angle [rad]')
 plt.ylabel('Virtual Torque [Nm]')
