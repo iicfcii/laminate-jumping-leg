bf0285688e6c42a8ecc2ec20fee63b5ccab63cf1
iicfcii <8230152+iicfcii@users.noreply.github.com>
2022-02-09 13:27:52 -0700

    Added mass error

--- anchor/opt.py
+++ anchor/opt.py
@@ -32,7 +32,7 @@ def cb(x,convergence=0):
     print('x',x)
     print('Convergence',convergence)
 
-def obj_stiffness(x,plot=False):
+def obj_stiffness(x,m,plot=False):
     try:
         datum = fourbar.stiffness(x,cs,plot=plot)
     except AssertionError:
@@ -58,18 +58,26 @@ def obj_stiffness(x,plot=False):
 
     e = np.sqrt(np.sum((f-f_d)**2)/f_d.shape[0])
 
+    mass_spring = (
+        (x[0]+x[2]+x[3])*fourbar.tr*fourbar.wr*fourbar.rho+
+        x[1]*fourbar.tf*x[4]*fourbar.rho
+    )
+    em = np.abs(m-mass_spring)
+
     if plot:
         plt.figure()
         plt.plot(x_d,f_d)
         # plt.plot(x_d,f,'--')
         plt.plot(np.array(datum['x'])*cs['r'],np.array(datum['f'])/cs['r'],'--')
-    return e
+        print('mass error',em)
+
+    return e+em
 
 cs = {
     'g': 9.81,
     'mb': 0.03,
     'ml': 0.0001,
-    'k': 30,
+    'k': 50,
     'a': 1,
     'ds': 0.05,
     'tau': 0.109872,
@@ -80,17 +88,20 @@ cs = {
 sol = jump.solve(cs)
 cs['dsm'] = -np.min(sol.y[1,:]) # spring travel range that needs to be matched
 bounds_motion = [(-np.pi,np.pi)]+[(0.02,0.06)]*5+[(-1,1)]*1
-bounds_stiffness = [(0.01,0.1)]*4+[(0.01,0.04)]+[(-1,1)]*1
+bounds_stiffness = [(0.01,0.08)]*4+[(0.01,0.03)]+[(-1,1)]*1
 
 if __name__ == '__main__':
     xm = None
     xs = None
     xm = [2.6935314437637747, 0.030244462243688645, 0.04668319649977162, 0.02002235749858264, 0.05998841948291793, 0.059996931859852574, 0.14061111190360398]
 
-    # a = 1, 0.3, 1.5
-    # xs = [6.970085451959984e-06, 0.05226738296756783, 0.05429323856448088, 0.07139280469863389, 0.010012365357000428]
+    # k = 30, a = 1, 0.3, 1.5
+    # xs = [0.010000772969153152, 0.06875649447318696, 0.05318142893986607, 0.09999526264404396, 0.010006773705780582, -0.5712798054675297]
     # xs = [0.07826743060011077, 0.02578101672467362, 0.09400397321290366, 0.012675774842643789, 0.010024264245079503, -0.7052606740094748]
-    # xs = [7.024017432230578e-06, 0.09999434872218227, 0.065678171577909, 0.035168059578512254, 0.010000828244387612]
+    # xs = [0.010074971479279167, 0.09999052165792412, 0.09953111001240444, 0.07628227908258375, 0.01001193333881196, 0.7404141028321847]
+
+    # k = 50, a = 1
+    xs = [0.010006806410504887, 0.04627745489917783, 0.0281204554460621, 0.06394194293073623, 0.010000796922791955, -0.1384887158574043]
 
     if xm is None:
         res = differential_evolution(
@@ -109,10 +120,12 @@ if __name__ == '__main__':
         print('Cost', res.fun)
         xm = res.x
 
+    mass_spring = cs['mb']-0.02-np.sum(xm[1:6])*fourbar.tr*fourbar.wr*fourbar.rho
     if xs is None:
         res = differential_evolution(
             obj_stiffness,
             bounds=bounds_stiffness,
+            args=(mass_spring,),
             popsize=10,
             maxiter=500,
             tol=0.01,
@@ -127,5 +140,5 @@ if __name__ == '__main__':
         xs = res.x
 
     print(obj_motion(xm,plot=True))
-    print(obj_stiffness(xs,plot=True))
+    print(obj_stiffness(xs,mass_spring,plot=True))
     plt.show()
