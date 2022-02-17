d56135c89289518a06028279a75452d0dabcfee6
iicfcii <8230152+iicfcii@users.noreply.github.com>
2022-02-17 09:29:52 -0700

    Updated k label

--- template/plot_k.py
+++ template/plot_k.py
@@ -4,26 +4,27 @@ import numpy as np
 
 cs = {
     'g': 9.81,
-    'mb': 0.03,
+    'mb': 0.025,
     'ml': 0.0001,
-    'k': 40,
+    'k': 50,
     'a': 1,
-    'ds': 0.05, # max leg extension
-    'tau': 0.109872,
-    'v': 30.410616886749196,
+    'ds': 0.05,
+    'tau': 0.15085776558260747,
+    'v': 47.75363911922214,
     'dl': 0.06,
     'r': 0.05
 }
-x0 = [0,0,0,0]
 
-ys = np.linspace(-cs['ds'],cs['ds'],100)
-A = [0.5,1,1.5]
+ys = np.linspace(0,cs['ds'],100)
+A = [0.6,0.8,1,1.2,1.4]
 lines = []
 for i, a in enumerate(A):
     cs['a'] = a
-    f_spring = np.sign(ys)*cs['k']*cs['ds']*np.power(np.abs(ys/cs['ds']),cs['a'])
-    plt.plot(ys,f_spring,label='a={:.1f}'.format(a))
+    f = -jump.f_spring(ys,cs)
+    plt.plot(ys,f,label='a={:.1f}'.format(a))
 plt.legend()
-plt.xlabel('y [m]')
-plt.ylabel('F [N]')
+plt.xlabel('Deformation')
+plt.ylabel('Force')
+plt.xticks([0,cs['ds']],['0','$\Delta_x$'])
+plt.yticks([0,f[-1]],['0','$k\Delta_x$'])
 plt.show()
