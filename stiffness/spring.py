896a672a82d4a85b2fb9fdba2702b6563a64f7f2
iicfcii <8230152+iicfcii@users.noreply.github.com>
2022-03-13 13:32:53 -0700

    Updated spring plot

--- stiffness/spring.py
+++ stiffness/spring.py
@@ -16,42 +16,45 @@ def read(s,k,a,n):
     rz[rz < 0] += 2*np.pi
 
     # Force bias
-    to = np.nonzero(t > 1)[0][0]
-    tz_offset = np.average(tz[:to])
-
-    # Select inital point
+    tz_offset = np.average(tz[t<3])
     tz = tz-tz_offset
+
+    # Set dir
     dir = np.sign(tz[-1])
     tz = dir*tz
     rz = -dir*rz
-    i = np.nonzero(tz > 0.001)[0][0]
-    tz = tz[i:]
-    rz = rz[i:]
+
+    # Find contact time
+    ti = t[np.nonzero(tz > 0.0002)[0][0]]
+
+    # ti = 3
+    # while ti < t[-1]:
+    #     idx = np.logical_and(t > ti,t < ti+1)
+    #     std = np.std(tz[idx])
+    #     if std > 0.001: break
+    #     ti += 0.1
+    # assert ti < t[-1]
+
+    # Select range
+    tz = tz[t>ti]
+    rz = rz[t>ti]
     rz = rz-rz[0]
-
-    # # Fit
-    # k = np.linalg.lstsq(rz.reshape((-1,1)),tz,rcond=None)[0][0]
-
-    # print(k/0.06**2)
-    # plt.plot(rz,tz)
-    # plt.show()
 
     return rz,tz
 
 r = 0.06
 d = 0.06
 if __name__ == '__main__':
-    for i,k in enumerate([70]):
+    plt.figure()
+    for i,k in enumerate([30,70]):
+        c = 'C{:d}'.format(i)
         for j,a in enumerate([0.7,1,1.5]):
-            c = 'C{:d}'.format(j)
             for s in [1]:
                 for n in [1,2,3]:
                     rz,tz = read(s,k,a,n)
-                    plt.plot(rz*r,tz/r,'.',color=c,markersize=1)
+                    plt.plot(rz*r,tz/r,'.',color=c,markersize=0.1)
 
             rzp = np.linspace(0,rz[-1],100)
             tzp = -jump.f_spring(rzp*r,k,a,d)*r
             plt.plot(rzp*r,tzp/r,color=c)
-    plt.xlabel('Stiffness Coefficient (N/m)')
-    plt.ylabel('Nonlinearity')
     plt.show()
