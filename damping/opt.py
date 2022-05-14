8a2867884817d2a2c3abaff6e75a1c940635c2c0
iicfcii <8230152+iicfcii@users.noreply.github.com>
2022-05-14 12:34:21 -0700

    Added match damping

--- damping/opt.py
+++ damping/opt.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 from utils import data
 from template import jump
+from . import sim
 
 def center(plot=False):
     def obj(x):
@@ -41,32 +42,101 @@ def center(plot=False):
 
     return xc,yc,r
 
-xc,yc,r = center(plot=False)
+def read(k,a,n,c,plot=False):
+    xc,yc,r = c
+
+    # Read data
+    d = data.read('./data/damping/damping_{:d}_{:d}_{:d}_{:d}.csv'.format(1,int(k*100),int(a*10),n))
+    t = np.array(d['t'])
+    xm = np.array(d['x'])
+    ym = np.array(d['y'])
+
+    # Convert to angle
+    theta = np.arctan2(ym-yc,xm-xc)
+    theta0 = np.mean(theta[t > 3])
+    theta -= theta0
+
+    # Select range
+    dtheta = np.concatenate([[0],(theta[1:]-theta[:-1])/(t[1:]-t[:-1])])
+    i = np.nonzero(dtheta<-10)[0][0]-1
+    j = np.nonzero(t > t[i]+sim.tfinal)[0][0]
+    tp = t[i:j]
+    thetap = theta[i:j]
+    dthetap = dtheta[i:j]
+
+    if plot:
+        plt.figure('theta')
+        plt.plot(t,theta)
+        plt.plot(tp,thetap)
+
+    tp -= tp[0]
+    return tp,thetap
+
+def obj(x,d,plot=False):
+    k,b = x
+    e = 0
+
+    for i, (t_e,theta_e) in enumerate(d):
+        cs = {
+            'k': k,
+            'b': b,
+            'I': sim.cs['I'],
+            'theta0': theta_e[0]
+        }
+        sol = sim.solve(cs)
+        t = sol.t
+        theta = sol.y[0,:]
+        theta_e = np.interp(t,t_e,theta_e)
+
+        e += np.sqrt(np.mean((theta-theta_e)**2))
+
+        if plot:
+            plt.figure('match')
+            plt.subplot(3,1,i+1)
+            plt.plot(t,theta)
+            plt.plot(t,theta_e)
+
+    return e
+
+def cb(x,convergence=0):
+    print('x',x)
+    print('Convergence',convergence)
 
 k = 0.2
 a = 2
-n = 1
+bounds=[(0,0.3),(0,0.001)]
+x = None
 
-# Read data
-d = data.read('./data/damping/damping_{:d}_{:d}_{:d}_{:d}.csv'.format(1,int(k*100),int(a*10),n))
-t = np.array(d['t'])
-xm = np.array(d['x'])
-ym = np.array(d['y'])
+# k=0.1 a=0.5,1,2
+# x = [0.036696747822411474, 2.9593453947478416e-05]
 
-# Convert to angle
-theta = np.arctan2(ym-yc,xm-xc)
-theta0 = np.mean(theta[t > 3])
-theta -= theta0
+# k=0.2 a=0.5,1,2
+x = [0.06043227741613831, 7.067560971637805e-05]
 
-# Select range
-dtheta = np.concatenate([[0],(theta[1:]-theta[:-1])/(t[1:]-t[:-1])])
-i = np.nonzero(dtheta<-10)[0][0]-1
-j = np.nonzero(t > t[i]+1)[0][0]
-tp = t[i:j]
-thetap = theta[i:j]
-dthetap = dtheta[i:j]
+if __name__ == '__main__':
+    c = center(plot=False)
+    d = []
+    for n in [1,2,3]:
+        t_e, theta_e = read(k,a,n,c)
+        d.append([t_e, theta_e])
 
-plt.figure('theta')
-plt.plot(t,theta)
-plt.plot(tp,thetap)
-plt.show()
+    if x is None:
+        res = differential_evolution(
+            obj,
+            bounds=bounds,
+            args=(d,),
+            popsize=10,
+            maxiter=500,
+            tol=0.001,
+            callback=cb,
+            workers=-1,
+            polish=False,
+            disp=True
+        )
+        print('Result', res.message)
+        print('x',str(list(res.x)))
+        print('Cost', res.fun)
+        x = res.x
+
+    print(obj(x,d,plot=True))
+    plt.show()
---
+++ damping/sim.py
@@ -0,0 +1,44 @@
+from sympy import *
+from scipy.integrate import solve_ivp
+import matplotlib.pyplot as plt
+
+step = 1e-3
+tfinal = 1
+
+k,b,I, = symbols('k b I')
+theta,dtheta = symbols('thtea dtheta')
+
+ddthtea = (-k*theta-b*dtheta)/I
+
+x = Matrix([theta, dtheta])
+dx = Matrix([dtheta, ddthtea])
+
+cs = {
+    'k': 0.1,
+    'b': 0.0001,
+    'I': 0.01*0.018**2,
+    'theta0': 0.4
+}
+
+def solve(cs,plot=False):
+    dx_f = lambdify(x,dx.subs(cs))
+
+    def spin(t, x):
+        return dx_f(*x).flatten()
+
+    sol = solve_ivp(spin, [0,tfinal], [cs['theta0'],0], max_step=step)
+
+
+    if plot:
+        plt.figure()
+        plt.subplot(211)
+        plt.plot(sol.t,sol.y[0,:])
+        plt.ylabel('theta')
+        plt.subplot(212)
+        plt.plot(sol.t,sol.y[1,:])
+        plt.ylabel('dtheta')
+
+    return sol
+
+# solve(cs,plot=True)
+# plt.show()
